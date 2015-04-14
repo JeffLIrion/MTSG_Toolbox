02f729fd5b90ee3cbef7490ad2cd0e547e2d0dea
Jeff Irion <jlirion@ucdavis.edu>
2015-04-13 22:11:32 -0700

    Made dvec_Threshold.m compatible with multi-dimensional signals

--- Utilities/dvec_Threshold.m
+++ Utilities/dvec_Threshold.m
@@ -22,34 +22,34 @@ function [dvec,kept] = dvec_Threshold(dvec,SORH,keep,GP,BS)
 
 
 %% 0. Preliminaries
-N = length(dvec);
+[~,cols] = size(dvec);
 
 % if keep is given as a fraction
 if keep < 1-eps && keep > eps
-    kept = round(keep*N);
+    kept = round(keep*numel(dvec));
     
 % if keep is given as an integer
 else
     kept = round(keep);
-    if kept > N
-        kept = N;
+    if kept > numel(dvec)
+        kept = numel(dvec);
     elseif kept < 0
         kept = 1;
     end
 end
 
 % if keep is too large
-if kept >= N
-    kept = N;
+if kept >= numel(dvec)
+    kept = numel(dvec);
     return
 end
 
 
 %% 1. Threshold the coefficients
 if strcmpi(SORH,'s') || strcmpi(SORH,'soft')
-    [~,ind] = sort(abs(dvec),'descend');
+    [~,ind] = sort(abs(dvec(:)),'descend');
     T = abs(dvec(ind(kept+1)));
-    
+
     % if BS is given, then use tag information so that scaling coefficients
     % are not soft-thresholded
     if nargin == 5
@@ -59,15 +59,15 @@ if strcmpi(SORH,'s') || strcmpi(SORH,'soft')
             GP = GHWT_Core(GP);
             [~,~,tag] = ExtractData(GP);
         end
-        
+
         % convert the tag matrix to a vector corresponding to BS
-        tag = dmatrix2dvec(tag,GP,BS);
-        
+        tag = repmat(dmatrix2dvec(tag,GP,BS),1,cols);
+
         % soft-threshold the non-scaling coefficients
-        ind = (1:N)';
+        ind = (1:numel(dvec))';
         ind = ind( tag > 0 );
         dvec(ind) = sign( dvec(ind) ) .* (abs( dvec(ind) )-T) .* (abs( dvec(ind) )-T > 0);
-        
+
     % if BS is not given, soft-threshold all coefficients
     else
         dvec = sign(dvec) .* (abs(dvec)-T) .* (abs(dvec)-T > 0);
@@ -76,12 +76,13 @@ if strcmpi(SORH,'s') || strcmpi(SORH,'soft')
     kept = nnz(dvec);
 
 elseif strcmpi(SORH,'h') || strcmpi(SORH,'hard')
-    [~,ind] = sort(abs(dvec),'descend');
-    dvec(ind(kept+1:N)) = 0;
+    [~,ind] = sort(abs(dvec(:)),'descend');
+    dvec(ind(kept+1:end)) = 0;
+    
     kept = nnz(dvec);
-
+    
 else
-    kept = N;
+    kept = numel(dvec);
 end
 
 
