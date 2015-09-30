f3bb48dedd8bd76c50cf41b82063946a65699aa2
Jeff Irion <jlirion@ucdavis.edu>
2015-09-30 14:28:16 -0700

    Simplified MTSG_Path.m

--- MTSG_Path.m
+++ MTSG_Path.m
@@ -8,37 +8,26 @@
 
 
 
-%% version info
-
+% version info
 MTSGVERSION = 001;
 fprintf('\nWelcome to the MTSG Toolbox v %g\n\n', MTSGVERSION);
 
 
-%% identify the type of computer and declare PATHNAMESEPARATOR, MTSGPATH, and MATLABPATHSEPARATOR accordingly
-
+% identify the type of computer and declare MTSGPATH accordingly
 Friend = computer;
-if strcmp(Friend,'MAC2')
-    PATHNAMESEPARATOR = ':';
-    MTSGPATH = ['Macintosh HD:Build:MTSG_Toolbox', num2str(MTSGVERSION), PATHNAMESEPARATOR];
-    MATLABPATHSEPARATOR = ';';
-elseif isunix
-    PATHNAMESEPARATOR = '/';
-    MTSGPATH = [pwd, PATHNAMESEPARATOR];
-    MATLABPATHSEPARATOR = ':';
-elseif strcmp(Friend(1:2),'PC')
-    PATHNAMESEPARATOR = '\';
-    MTSGPATH = [pwd, PATHNAMESEPARATOR];
-    MATLABPATHSEPARATOR = ';';
+if isunix || strcmp(Friend(1:2),'PC')
+    MTSGPATH = [fileparts(mfilename('fullpath')),filesep];
 else
     fprintf('\n\nI don''t recognize this computer.\nPathnames not set.\n\nSolution: edit MTSG_Path.m\n\n');
+    return
 end
 
 
-%% generate the path string ==> includes the folder and multiple levels of subfolders below it
-
+% generate the path string ==> includes the folder and multiple levels of subfolders below it
 V = version('-date');
 V = str2double(V(end-3:end));
 
+% make sure the Matlab version is 6.x or above
 if V < 2006
     fprintf('Warning: This version is only supported on Matlab 6.x or above');
     mtsgp=genpath(MTSGPATH,1);
@@ -47,3 +36,7 @@ else
 end
 
 addpath(mtsgp);
+
+
+% clear variables
+clear MTSGVERSION Friend MTSGPATH V mtsgp
