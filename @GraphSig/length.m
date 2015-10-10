50b2be69d4b91329d6e280cb69bdae0a96d81631
Jeff Irion <jlirion@ucdavis.edu>
2015-10-10 14:48:14 -0700

    Added @GraphSig/length.m

---
+++ @GraphSig/length.m
@@ -0,0 +1,22 @@
+function N = length(G)
+% Find the length of a GraphSig object (i.e., the number of vertices in the
+% graph)
+%
+% Input
+%   G       a GraphSig object
+%
+% Output
+%   N       the length of the GraphSig object
+%
+%
+%
+% Copyright 2015 The Regents of the University of California
+%
+% Implemented by Jeff Irion (Adviser: Dr. Naoki Saito)
+
+
+
+N = G.length;
+
+
+end
